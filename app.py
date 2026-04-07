7ad7fc4c156943f66030acb2828c2f47dd74d3b2
Ankan Paul <paulankan1001@gmail.com>
2026-04-07 22:57:21 +0530

    Changed the title

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ model = joblib.load("KNN_heart.pkl")
 scaler = joblib.load("scaler.pkl")
 expected_columns = joblib.load("columns.pkl")
 
-st.title("Heart Stroke Prediction by ANKAN")
-st.markdown("Provide the following details to check your heart stroke risk:")
+st.title("Heart Disease Prediction Model by ANKAN PAUL")
+st.markdown("Provide the following details to check your heart disease risk:")
 
 # Collect user input
 age = st.slider("Age", 18, 100, 40)
